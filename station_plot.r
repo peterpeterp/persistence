79fd33b93e44630a94a6963eb6832c940fe41d92
peterpeterp <peterpfleiderer@gmx.de>
2015-07-16 16:33:52 +0200

    station plot ist fuer 2 states wieder benutzbar

--- station_plot.r
+++ station_plot.r
@@ -2,37 +2,39 @@ source("write.r")
 source("load.r")
 source("functions_duration.r")
 
-station_plot <- function(dat,trend,per,dur_name,warmeTageIncr,q,start,stop,filename){
+station_plot <- function(dat,trend,per,dur_name,warmeTageIncr,q,filename){
     pdf(file=filename)
 
     station=which(dat$ID==q)[1]
 
-    season_names=c("spring","summer","autumn","winter","year")
+    season_names=c("spring","summer","autumn","winter")
     season_starts=c(59,151,242,335,1)
     season_stops=c(151,242,335,425,1)
-    for (sea in 1:5){
+    for (sea in 1:4){
         par(mfrow=c(2,1))
         par(mar=c(2,2, 2, 0.5))
-        dur=duration_load(paste(dur_name,season_names[sea],".nc",sep="")) 
+        nc=open.ncdf(paste(dur_name,season_names[sea],".nc",sep="")) 
+        dur=get.var.ncdf(nc,"dur")
+        dur_mid=get.var.ncdf(nc,"dur_mid")
 
-        plot(NA,xlim=c(1950,2010),xaxp=c(1950,2010,12),ylim=c(0,50),xlab="",ylab="duration of persistence")#, main="summer warm persistence")
-        points(dur$dur_warm_mid[station,],dur$dur_warm[station,],pch=4,col="red")
+        plot(NA,xlim=c(1950,2014),xaxp=c(1950,2015,13),ylim=c(0,50),xlab="",ylab="duration of persistence")#, main="summer warm persistence")
+        points(dur_mid[station,2,],dur[station,2,],pch=4,col="red")
         markov=per$markov[station,sea,4,]*30
         lines(dat$year+0.5,markov,col="green")
-        for (i in seq(1950,2010,5)){
+        for (i in seq(1950,2015,5)){
             abline(v=i,col="gray",lty="dotted")
         }
-        lines(dat$time[start:stop],as.vector(trend[station,,])[start:stop],col="black")
+        lines(dat$time,as.vector(trend[station,,]),col="black")
         legend("topleft", pch = c(NA,4,NA,NA,NA),lty=c(NA,NA,1,NA,1), col = c(NA,"red", "green",NA,"black"), 
                 legend = c(season_names[sea],"warm days in a row","markov cold persistence",
                     sprintf("warm day increase per year %.3f",warmeTageIncr[q,sea]),"trend"))
 
-        plot(NA,xlim=c(1950,2011),xaxp=c(1950,2010,12),ylim=c(0,50),xlab="",ylab="duration of persistence")#, main="summer cold persistence")
-        points(dur$dur_cold_mid[station,],dur$dur_cold[station,],pch=5,col="blue")
+        plot(NA,xlim=c(1950,2014),xaxp=c(1950,2015,13),ylim=c(0,50),xlab="",ylab="duration of persistence")#, main="summer cold persistence")
+        points(dur_mid[station,1,],dur[station,1,],pch=5,col="blue")
         markov=per$markov[station,sea,1,]*30
         lines(dat$year,markov,col="green")
-        lines(dat$time[start:stop],as.vector(trend[station,,])[start:stop],col="black")
-        for (i in seq(1950,2010,5)){
+        lines(dat$time,as.vector(trend[station,,]),col="black")
+        for (i in seq(1950,2015,5)){
             abline(v=i,col="gray",lty="dotted")
         }
         legend("topleft", pch = c(NA,5,NA,NA,NA),lty=c(NA,NA,1,NA,1), col = c(NA,"blue", "green",NA,"black"), 
@@ -50,13 +52,13 @@ station_plot <- function(dat,trend,per,dur_name,warmeTageIncr,q,start,stop,filen
             trenn=1980
             br=seq(0,100,2)
             ntot=819
-            vor_warm=which(dur$dur_warm_mid[q,]<trenn)
-            vor_cold=which(dur$dur_cold_mid[q,]<trenn)
-            nach_warm=which(dur$dur_warm_mid[q,]>trenn)
-            nach_cold=which(dur$dur_cold_mid[q,]>trenn)
+            vor_warm=which(dur_mid[q,2,]<trenn)
+            vor_cold=which(dur_mid[q,1,]<trenn)
+            nach_warm=which(dur_mid[q,2,]>trenn)
+            nach_cold=which(dur_mid[q,1,]>trenn)
 
-            hist1=hist(dur$dur_warm[q,vor_warm],breaks=br,plot=FALSE)
-            hist2=hist(dur$dur_warm[q,nach_warm],breaks=br,plot=FALSE)
+            hist1=hist(dur[q,2,vor_warm],breaks=br,plot=FALSE)
+            hist2=hist(dur[q,2,nach_warm],breaks=br,plot=FALSE)
 
 
             par(mfrow=c(1,2))
@@ -72,22 +74,22 @@ station_plot <- function(dat,trend,per,dur_name,warmeTageIncr,q,start,stop,filen
             fit=nls(y~exp(a+b*x),data=xy,start=list(a=0,b=0))
             yfit=exp(summary(fit)$parameters[1])*exp(x*summary(fit)$parameters[2])
             lines(x,yfit,col="green")
-            x5=(log(0.05)-summary(fit)$parameters[1])/summary(fit)$parameters[2]
-            abline(v=x5,col="green")
+            x2=(log(0.02)-summary(fit)$parameters[1])/summary(fit)$parameters[2]
+            abline(v=x2,col="green")
 
             x=hist2$mids
             xy=data.frame(y=hist2$density,x=x)
             fit=nls(y~exp(a+b*x),data=xy,start=list(a=0,b=0))
             yfit=exp(summary(fit)$parameters[1])*exp(x*summary(fit)$parameters[2])
             lines(x,yfit,col="red")
-            x5=(log(0.05)-summary(fit)$parameters[1])/summary(fit)$parameters[2]
-            abline(v=x5,col="red")
+            x2=(log(0.02)-summary(fit)$parameters[1])/summary(fit)$parameters[2]
+            abline(v=x2,col="red")
 
             legend("topright", pch = c(15,15), col = c("green", "red"), 
                     legend = c(paste("before",trenn),paste("after",trenn)))
         
-            hist1=hist(dur$dur_cold[q,vor_cold],breaks=br,plot=FALSE)
-            hist2=hist(dur$dur_cold[q,nach_cold],breaks=br,plot=FALSE)
+            hist1=hist(dur[q,1,vor_cold],breaks=br,plot=FALSE)
+            hist2=hist(dur[q,1,nach_cold],breaks=br,plot=FALSE)
 
             endx=max(which(hist1$counts!=0),which(hist2$counts!=0))+1
           
@@ -128,7 +130,7 @@ ndays = c(91)
 nyrs = c(5)
 
 stations=c(488,510,604,744,920,887,251,98,270,281,169,164,353,121,11,39)
-stations=c(488)
+stations=c(744)
 for (nday in ndays){
     for (nyr in nyrs){
         for (qq in stations){
@@ -140,7 +142,7 @@ for (nday in ndays){
 
 
             station_plot(dat=dat,trend=trend,per=per,dur_name=sprintf("../data/%s_%s/%s_%s_duration_2s_",nday,nyr,nday,nyr),
-                warmeTageIncr=warmeTageIncr,q=qq,start=365*(58),stop=365*(65),
+                warmeTageIncr=warmeTageIncr,q=qq,
                 filename=sprintf("../plots/station/%s_%s_station_test_%s.pdf",nday,nyr,qq))
         }
     }
