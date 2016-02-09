405aff0695680c403f5d2b796b96c5ec405fc583
peter <peter@localhost.localdomain>
2016-02-09 17:28:24 +0100

    aufgeraeumt fuers clustern und criterien noch nicht getestet

--- clustering.r
+++ clustering.r
@@ -43,12 +43,10 @@ dissimilarity_matrix <- function(lagMax=3,ID_select=1:1319,timeRange=4000:11000,
         }
         print(proc.time())
     }
-    
 
     # to many nas -> flat 
     X[is.na(X)]=0
-    X<<-X
-    adasd
+    #X<<-X
 
     # cross correlation
     dissMat=array(NA,c(IDlength,IDlength))
@@ -90,122 +88,117 @@ dissimilarity_matrix <- function(lagMax=3,ID_select=1:1319,timeRange=4000:11000,
     close.nc(nc_out)     
 }
 
-dissimilarity_view <- function(lagMax=16,nGroup=12,load_name="_Cor",add_name="",method=method){
+dissimilarity_view <- function(lagMax=16,nGroup=12,load_name="_Cor",add_name="",method=method,auswahl=c(333,470,604,890,888)){
     print(paste("../data/",trendID,"/",dataset,additional_style,"/clustering/",period,"/",trendID,"_",period,"_distance_",lagMax,load_name,".nc",sep=""))
     nc=open.nc(paste("../data/",trendID,"/",dataset,additional_style,"/clustering/",period,"/",trendID,"_",period,"_distance_",lagMax,load_name,".nc",sep=""))
     distMat<<-var.get.nc(nc,"distanceMat")
     choiceMat<<-var.get.nc(nc,"choiceMat")
     distMat[is.na(distMat)]=1
     ntot<<-length(ID_select)
-    print(proc.time())
-    
-    #auswahl=c(1250,1171,1148,907,980,692,695,481,479)
-    #auswahl=c(333,470,604,890,888)
-    #reihen=choiceMat[auswahl,]
-    #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,add_name,"_best_lag.pdf",sep=""),reihen=reihen,farb_mitte=0,farb_palette="lila-gruen",highlight_points=auswahl,highlight_color="red",paper=c(8,10),ausschnitt=c(30,70),layout_mat=matrix(c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6),nrow=16),pointsize=1.5)
+
+    reihen=choiceMat[auswahl,]
+    topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,add_name,"_best_lag.pdf",sep=""),reihen=reihen,farb_mitte=0,farb_palette="lila-gruen",highlight_points=auswahl,highlight_color="red",paper=c(8,10),ausschnitt=c(30,70),layout_mat=matrix(c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6),nrow=16),pointsize=1.5)
     #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,load_name,add_name,"_groups_",nGroup,"_",method,"_best_lag.pdf",sep=""),reihen=reihen,farb_palette="lila-gruen",highlight_points=auswahl,highlight_color="red")
 
-    #reihen=distMat[auswahl,]
-    #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,add_name,"_distance.pdf",sep=""),reihen=reihen,farb_mitte=c(0,1),farb_palette="regenbogen",highlight_points=auswahl,highlight_color="red",paper=c(8,10),ausschnitt=c(30,70),layout_mat=matrix(c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6),nrow=16),pointsize=1.5)
+    reihen=distMat[auswahl,]
+    topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,add_name,"_distance.pdf",sep=""),reihen=reihen,farb_mitte=c(0,1),farb_palette="regenbogen",highlight_points=auswahl,highlight_color="red",paper=c(8,10),ausschnitt=c(30,70),layout_mat=matrix(c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6),nrow=16),pointsize=1.5)
     #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,load_name,add_name,"_groups_",nGroup,"_",method,"_distance.pdf",sep=""),reihen=reihen,farb_palette="regenbogen")
 
-    if (method!="kmeans"){
-        tmp<<-hclust(as.dist(distMat[ID_select,ID_select]),method=method)
-        reihen=array(NA,c(8,1319))
-        reihen_sig=array(NA,c(8,1319))
-        for (plot in 1:8){
-            nGroup=plot+18
-            same=array(1,nGroup)
-            reihen[plot,ID_select]=cutree(tmp,nGroup)
-            if (plot>1){
-                for (G in 1:nGroup){
-                    GG<<-reihen[plot-1,which(reihen[plot,]==G)[1]]
-                    if (length(which(reihen[plot,]==G))==length(which(reihen[plot-1,]==GG))){same[G]=0}
-                }
-                reihen_sig[plot-1,which(reihen[plot,] %in% (1:nGroup)[which(same==1)])]=-2
-            }
-        }
-    }
-
-    if (method=="kmeans"){
-        reihen=array(NA,c(30,1319))
-        reihen_sig=array(NA,c(30,1319))
-        for (plot in 1:30){
-            nGroup=plot
-            same=array(1,nGroup)
-            reihen[plot,ID_select]=pam(distMat[ID_select,ID_select],nGroup)$clustering
-            if (plot>1){
-                for (G in 1:nGroup){
-                    GG<<-reihen[plot-1,which(reihen[plot,]==G)[1]]
-                    if (length(which(reihen[plot,]==G))==length(which(reihen[plot-1,]==GG))){same[G]=0}
-                }
-                reihen_sig[plot-1,which(reihen[plot,] %in% (1:nGroup)[which(same==1)])]=-2
-            }
-        }
-    }
-
-
-    #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,load_name,add_name,"_groups_",nGroup,"_",method,"_map.pdf",sep=""),reihen=reihen,reihen_sig=reihen_sig,farb_palette="viele")
-    topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,load_name,add_name,"_groups_",nGroup,"_",method,"_map.pdf",sep=""),reihen=reihen,reihen_sig=reihen_sig,pch_style=reihen,farb_palette="viele",paper=c(8,12),ausschnitt=c(30,70),layout_mat=matrix(c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9),nrow=17),pointsize=1.5)
 }
 
-hcluster_view <- function(lagMax=16,nGroup=12,load_name="_Cor",add_name="",method=method){
+cluster_evaluation <- function(method="ward.D2",untilGroup=30,add_name="",ID_select=1:1319,load_name="_Cor",lagMax=15,timeRange=4000:11000,normalize=FALSE){
     print(paste("../data/",trendID,"/",dataset,additional_style,"/clustering/",period,"/",trendID,"_",period,"_distance_",lagMax,load_name,".nc",sep=""))
     nc=open.nc(paste("../data/",trendID,"/",dataset,additional_style,"/clustering/",period,"/",trendID,"_",period,"_distance_",lagMax,load_name,".nc",sep=""))
     distMat<<-var.get.nc(nc,"distanceMat")
-    choiceMat<<-var.get.nc(nc,"choiceMat")
     distMat[is.na(distMat)]=1
     ntot<<-length(ID_select)
-    print(proc.time())
-    
-    #auswahl=c(1250,1171,1148,907,980,692,695,481,479)
-    #auswahl=c(333,470,604,890,888)
-    #reihen=choiceMat[auswahl,]
-    #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,add_name,"_best_lag.pdf",sep=""),reihen=reihen,farb_mitte=0,farb_palette="lila-gruen",highlight_points=auswahl,highlight_color="red",paper=c(8,10),ausschnitt=c(30,70),layout_mat=matrix(c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6),nrow=16),pointsize=1.5)
-    #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,load_name,add_name,"_groups_",nGroup,"_",method,"_best_lag.pdf",sep=""),reihen=reihen,farb_palette="lila-gruen",highlight_points=auswahl,highlight_color="red")
 
-    #reihen=distMat[auswahl,]
-    #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,add_name,"_distance.pdf",sep=""),reihen=reihen,farb_mitte=c(0,1),farb_palette="regenbogen",highlight_points=auswahl,highlight_color="red",paper=c(8,10),ausschnitt=c(30,70),layout_mat=matrix(c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6),nrow=16),pointsize=1.5)
-    #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,load_name,add_name,"_groups_",nGroup,"_",method,"_distance.pdf",sep=""),reihen=reihen,farb_palette="regenbogen")
+    # X is required for cluster Criteria
+    # X is evaluated as in dissimilarity_matrix
+    X=array(dat$tas,c(1319,365*65))
+    # range in which data coverage is best
+    X=X[ID_select,timeRange]
+    # remove nas problematic
+    xLen=length(timeRange)
+
+    if (normalize){
+        # "normalize" using quantiles
+        for (q in ID_select){
+            if (length(which(!is.na(X[q,])))>xLen/2){
+                X[q,]=X[q,]/(quantile(x=X[q,],probs=0.95,na.rm=TRUE)-quantile(x=X[q,],probs=0.05,na.rm=TRUE))
+            }
+            else{X[q,]=NA}
+        }
+    }
+    # to many nas -> flat 
+    X[is.na(X)]=0
+    X<<-X
+
+    attribution=array(NA,c(untilGroup,1319))
+    attribution_changes=array(NA,c(untilGroup,1319))
+    criteria=array(NA,c(untilGroup,100))
 
     if (method!="kmeans"){
         tmp<<-hclust(as.dist(distMat[ID_select,ID_select]),method=method)
-        reihen=array(NA,c(8,1319))
-        reihen_sig=array(NA,c(8,1319))
-        for (plot in 1:8){
-            nGroup=plot+18
+        for (nGroup in 1:untilGroup){
             same=array(1,nGroup)
-            reihen[plot,ID_select]=cutree(tmp,nGroup)
-            if (plot>1){
+            attribution[nGroup,ID_select]=cutree(tmp,nGroup)
+            critTmp=intCriteria(X[ID_select,],attribution[nGroup,ID_select],"all")
+            criteria[nGroup,1:length(critTmp)]=critTmp
+            if (nGroup>1){
                 for (G in 1:nGroup){
-                    GG<<-reihen[plot-1,which(reihen[plot,]==G)[1]]
-                    if (length(which(reihen[plot,]==G))==length(which(reihen[plot-1,]==GG))){same[G]=0}
+                    GG<<-attribution[nGroup-1,which(attribution[nGroup,]==G)[1]]
+                    if (length(which(attribution[nGroup,]==G))==length(which(attribution[nGroup-1,]==GG))){same[G]=0}
                 }
-                reihen_sig[plot-1,which(reihen[plot,] %in% (1:nGroup)[which(same==1)])]=-2
+                attribution_changes[nGroup-1,which(attribution[nGroup,] %in% (1:nGroup)[which(same==1)])]=-2
             }
         }
     }
 
     if (method=="kmeans"){
-        reihen=array(NA,c(30,1319))
-        reihen_sig=array(NA,c(30,1319))
-        for (plot in 1:30){
-            nGroup=plot
+        for (nGroup in 1:30){
             same=array(1,nGroup)
-            reihen[plot,ID_select]=pam(distMat[ID_select,ID_select],nGroup)$clustering
-            if (plot>1){
+            attribution[nGroup,ID_select]=pam(distMat[ID_select,ID_select],nGroup)$clustering
+            intCriteria(X[ID_select,],attribution[nGroup,ID_select],"all")
+            if (nGroup>1){
                 for (G in 1:nGroup){
-                    GG<<-reihen[plot-1,which(reihen[plot,]==G)[1]]
-                    if (length(which(reihen[plot,]==G))==length(which(reihen[plot-1,]==GG))){same[G]=0}
+                    GG<<-attribution[nGroup-1,which(attribution[nGroup,]==G)[1]]
+                    if (length(which(attribution[nGroup,]==G))==length(which(attribution[nGroup-1,]==GG))){same[G]=0}
                 }
-                reihen_sig[plot-1,which(reihen[plot,] %in% (1:nGroup)[which(same==1)])]=-2
+                attribution_changes[nGroup-1,which(attribution[nGroup,] %in% (1:nGroup)[which(same==1)])]=-2
             }
         }
     }
 
+    nc_out<-create.nc(paste("../data/",trendID,"/",dataset,additional_style,"/clustering/",period,"/",trendID,"_",period,"_distance_",lagMax,add_name,"_",method,"_",untilGroup,".nc",sep=""))
+    att.put.nc(nc_out, "NC_GLOBAL", "ID_explanation", "NC_CHAR", "gridpoints")
+    att.put.nc(nc_out, "NC_GLOBAL", "period", "NC_CHAR", period)
+    att.put.nc(nc_out, "NC_GLOBAL", "explanation", "NC_CHAR", "different cuttrees with indices etc")
+    
+    dim.def.nc(nc_out,"ID",dimlength=1319, unlim=FALSE)   
+    dim.def.nc(nc_out,"clusters",dimlength=untilGroup, unlim=FALSE)   
+    dim.def.nc(nc_out,"indices",dimlength=     , unlim=FALSE)   
 
-    #topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,load_name,add_name,"_groups_",nGroup,"_",method,"_map.pdf",sep=""),reihen=reihen,reihen_sig=reihen_sig,farb_palette="viele")
-    topo_map_plot(filename_plot=paste("../plots/",trendID,"/",dataset,additional_style,"/clustering/lag_",lagMax,load_name,add_name,"_groups_",nGroup,"_",method,"_map.pdf",sep=""),reihen=reihen,reihen_sig=reihen_sig,pch_style=reihen,farb_palette="viele",paper=c(8,12),ausschnitt=c(30,70),layout_mat=matrix(c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9),nrow=17),pointsize=1.5)
+    var.def.nc(nc_out,"attribution","NC_DOUBLE",c(1,0))
+    att.put.nc(nc_out, "attribution", "missing_value", "NC_DOUBLE", -99999.9)
+    att.put.nc(nc_out, "attribution", "dim_explanation", "NC_CHAR", "nClusters-ID")
+    att.put.nc(nc_out, "attribution", "explanation", "NC_CHAR", "attributions")
+
+    var.def.nc(nc_out,"attribution_changes","NC_DOUBLE",c(1,0))
+    att.put.nc(nc_out, "attribution_changes", "missing_value", "NC_DOUBLE", -99999.9)
+    att.put.nc(nc_out, "attribution_changes", "dim_explanation", "NC_CHAR", "nClusters-ID")
+    att.put.nc(nc_out, "attribution_changes", "explanation", "NC_CHAR", "changes with respect to previous number of clusters")
+
+    var.def.nc(nc_out,"criteria","NC_DOUBLE",c(1,2))
+    att.put.nc(nc_out, "criteria", "missing_value", "NC_DOUBLE", -99999.9)
+    att.put.nc(nc_out, "criteria", "dim_explanation", "NC_CHAR", "nClusters-indices")
+    att.put.nc(nc_out, "criteria", "explanation", "NC_CHAR", "intCriteria - all - indices")
+
+    var.put.nc(nc_out,"attribution",dissMat)      
+    var.put.nc(nc_out,"attribution_changes",dissMat)      
+    var.put.nc(nc_out,"criteria",choiceMat)      
+ 
+    close.nc(nc_out)         
 }
 
 init <- function(){
@@ -234,19 +227,20 @@ init <- function(){
 
 
 
-ID_select=which(dat$lat>=30 & dat$lat<=70 )
-ID_select=1:1319
+
+
 
 
 init()
 
 
 
-dissimilarity_matrix(lagMax=15,add_name="_Cor",normalize=FALSE)
+#dissimilarity_matrix(lagMax=15,add_name="_Cor",normalize=FALSE)
 
-dissimilarity_view(lagMax=15,load_name="_Cor",add_name="_ww",method="ward.D2")
+#dissimilarity_view(lagMax=15,load_name="_Cor",add_name="_ww",method="ward.D2")
 
-
+cluster_evaluation(add_name="_ww",ID_select=1:1319)
+#cluster_evaluation(add_name="_ml",ID_select=which(dat$lat>=30 & dat$lat<=70 ))
 
 
 
