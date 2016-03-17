f37846f339ed62d6f60d74920bbd9ef322304caa
peter <peter@localhost.localdomain>
2016-03-17 11:33:34 +0100

    cleaned

--- station_conversion.py
+++ station_conversion.py
@@ -1,12 +1,5 @@
 
 
-#for yr in range(1950,2012):
-#	tmp=open("../data/raw_data/canda_P/Prnational"+str(yr)+".txt",'r').read()
-#	new=open("../data/raw_data/canda_P/P_canada"+str(yr)+".txt",'w')
-#	tmp=tmp.replace("-999","  -9")
-#	new.write(tmp)
-#	new.close
-
 def convert_station_files():
 	lines=open("../data/raw_data/ghcn/ghcnd-stations.txt","r").read().split("\n")
 	neu=open("../data/raw_data/ghcn/ghcnd-stations_all.txt","w")
@@ -45,20 +38,6 @@ def write_regional_selection(region_name="ward23",G="7"):
 		if i in reg_sel:
 			print line.split("\t")[1]
 			selection_IDs.write(str(i)+"\t"+line.split("\t")[1]+"\n")
-
-def reduce_precip_file_to_stations(yr="2013",region_name="ward23",G="7"):
-	selection_IDs=open("../data/raw_data/ghcn/ghcnd-stations_"+region_name+"_"+G+"_ID.txt","r").read().split("\n")
-	precip=open("../data/raw_data/ghcn/precip_"+yr+".txt","r").read().split("\n")
-
-	reduced=open("../data/raw_data/ghcn/precip_"+region_name+"_"+G+"_"+yr+".txt","w")
-
-	for line in precip:
-		#print line.split(" ")[1].split('"')[1]
-		if line.split(" ")[1].split('"')[1] in selection_IDs:
-			reduced.write(line+"\n")
-
-
-
 
 
 #convert_station_files()
